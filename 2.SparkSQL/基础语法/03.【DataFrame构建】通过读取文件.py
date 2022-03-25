8ad789cafa28b5f0d328a9bb68214f5f1bbb7c92
baby <zjb6975017@qq.com>
2022-03-25 20:04:38 +0800

    通过读取文件来构建DataFrame

--- 2.SparkSQL/基础语法/03.【DataFrame构建】通过读取文件.py
+++ 2.SparkSQL/基础语法/03.【DataFrame构建】通过读取文件.py
@@ -1,6 +1,7 @@
 # coding:utf8
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StringType
 
 if __name__ == '__main__':
     # Spark的入口
@@ -12,17 +13,24 @@ if __name__ == '__main__':
     # 可以使用使用sparkSession对象来生成SC对象
     sc = spark.sparkContext
 
-    # 读取学生分数表
-    rdd = sc.textFile("测试数据/stu_score.txt")
+    # 一、读取text文件数据源，读取特点是将一整行当一列读取，默认列明是value,类型是String
+    schema = StructType().add("data",StringType(),nullable=True)
+    df = spark.read.format("text").\
+        schema(schema = schema).\
+        load("测试数据/people.txt")
 
-    rdd = rdd.map(lambda x:x.split(",")).\
-        map(lambda x:(int(x[0]),x[1],int(x[2])))
+    # 二、读取json文件数据源，一般不用写.schema，json自带，json带有列名和列类型
+    df = spark.read.format("json").\
+        load("测试数据/people.txt")
 
-    # 通过crateDataFrame函数构造DataFrame
-    df = spark.createDataFrame(rdd,["id","name","score"])
+    # 三、读取csv文件数据源
+    df = spark.read.format("csv").\
+        option("sep",";").\
+        option("header",False).\
+        option("enconding","uft-8").\
+        schema("name STRING,age INT,job STRING").\
+        load("测试数据/people.json")
 
-    # 打印表结构
-    df.printSchema()
-
-    # 打印表内容
-    df.show()
+    # 四、读取parquet读数据
+    df = spark.read.format("parquet").\
+        load("测试数据/users.parquet")
